0619bc1243003f2b47d2f7eaeff87d5f8b84b498
RodrigoRamone7 <69323304+RodrigoRamone7@users.noreply.github.com>
2023-05-10 13:55:28 -0300

    Acessando janela pelo id

--- algoritmo.py
+++ algoritmo.py
@@ -5,15 +5,19 @@ def ripwin():
     window_true = None
     janelas = []
     janelas += gw.getAllTitles() #Array with all active windows
+    janelasid =[]
+    janelasid += gw.getAllWindows()
     
     tittle = 'RasterLinkPro5IP' #Título a ser encontrado
     for valid in janelas: #Valida se o raster link está aberta
         if tittle in valid:
             window_true = valid
             window_index = janelas.index(window_true)
+            window_id = janelasid[window_index]
             break
     if window_true:
         rasterlinkwindow = gw.getWindowsWithTitle(window_true)[0]
+        rasterlinkwindow.resizeTo(200, 200)
         
             
     else:
