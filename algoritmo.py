906ed88b6166bdbc5e8d3edc329fc5e16eafeb0e
RodrigoRamone7 <69323304+RodrigoRamone7@users.noreply.github.com>
2023-05-12 11:03:23 -0300

    Captura de tela

--- algoritmo.py
+++ algoritmo.py
@@ -1,28 +1,39 @@
-import pyautogui
+import pyautogui as ag
 import pygetwindow as gw
 
-def ripwin():
+def ripwin(): #Valida se a janela está aberta
     window_true = None
     janelas = []
-    janelas += gw.getAllTitles() #Array with all active windows
-    janelasid =[]
-    janelasid += gw.getAllWindows()
+    janelas += gw.getAllTitles()
     
     tittle = 'RasterLinkPro5IP' #Título a ser encontrado
     for valid in janelas: #Valida se o raster link está aberta
         if tittle in valid:
             window_true = valid
-            window_index = janelas.index(window_true)
-            window_id = janelasid[window_index]
             break
-    if window_true:
+    if window_true: # Se a janela estiver aberta vai abrir e redimencionar
         rasterlinkwindow = gw.getWindowsWithTitle(window_true)[0]
+        rasterlinkwindow.minimize()
         rasterlinkwindow.maximize()
         rasterlinkwindow.resizeTo(800, 800)
         
             
     else:
-        pyautogui.alert(text='Abra o programa de RIP', title='ERRO!', button='OK')
-        
+        ag.alert(text='Abra o programa de RIP', title='ERRO!', button='OK')
+        return False
+    
+    return True
 
-ripwin()
+def moveto(): #Sequencia de ações para encontrar níveis de tinta
+    if ripwin() == True: # A janela do RIP deve estar no monitor principal
+        ag.moveTo(170,115)
+        ag.click()
+        ag.moveTo(700,420)
+        ag.click()
+        return True
+        
+def capture(): #Captura Screenshot dos níveis de tinta
+    if moveto() == True:
+        img = ag.screenshot('ss.png',region=(489,165, 289, 232),)
+        
+capture()
